4f2988a1b48aa7678f88a731162c02bc4f050f33
daniel0514 <daniel0514@gmail.com>
2017-03-05 21:43:58 -0800

    added project pages. Added comments to childrenPage.js. Added pie chart to oneclickupload.htm

--- js/childrenPage.js
+++ js/childrenPage.js
@@ -1,76 +1,157 @@
+//Subcell Colors
 var colors = ['#CAEBF2', '#A9A9A9', '#FF3B3F', '#EFEFEF', '#96858F', '#6D7993', '#9099A2', '#D5D5D5', '#D7CEC7', '#FEDCD2', '#BFD8D2', '#DCB239'];
 
-
-
+//DOMContentLoaded listener is to replace jQuery's ready() function
 document.addEventListener('DOMContentLoaded', function(){
+    //When DOM Content is loaded (ready)
+    //Assign background color of subcells randomly.
     var elems = document.getElementsByClassName("subcell");
+    //Construct a new color array from original colors array so we can remove a color whenever
+    //a cell is assigned its color
     var colorsLeft = colors.slice();
     for(var i  = 0; i < elems.length; i++){
         const element = elems[i];
+        //Get random index
         var choice = getRandomInt(0, colorsLeft.length - 1);
+        //Assign the subcell the randomly chosen color
         element.style.background = colorsLeft[choice];
+        //Remove the color from the array so no subcells will have duplicated colors
         colorsLeft.splice(choice, 1);
-
-        //element.style.opacity = 0.75;
-        //element.addEventListener('mouseover', function(){
-        //    element.style.opacity = 1.0;
-        //});
-        //element.addEventListener('mouseout', function(){
-        //    element.style.opacity = 0.75;
-        //});
     }
-    //
-    //var elems = document.getElementsByClassName("project");
-    //for(var i = 0; i < elems.length; i++){
-    //    hoverContentSwap(elems[i], projects[i].htmlContentHover, projects[i].htmlContent);
-    //}
+
+    //Draw Pie Charts
+    //Get the canvas DOM element
+    var canvas = document.getElementsByClassName("technologyPie");
+    //Get the DIV element to put legend in
+    var legend = document.getElementById("technologyPieLegend");
+    //Construct the PieChart object
+    var piechart = new PieChart({
+        //The canvas DOM object
+        canvas: canvas[0],
+        //The Pie Split Data
+        data: getData(canvas[0].id),
+        //The colors to be used in the pie chart
+        colors:["#fde23e","#f16e23", "#57d9ff","#937e88"],
+        //The DIV DOM object to put legends in
+        legend: legend
+    });
+    piechart.draw();
 });
 
+
+var OneClickUpload = {
+    "Java" : 75,
+    "SQL" : 25
+}
+
+/**
+ * Simple function to get random integers between the minimum and the maximum values
+ * @param min : the minimum value
+ * @param max : the maximum value
+ * @returns {number} : a random integer between min and max
+ */
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-//
-//function hoverContentSwap(elem, mouseOverContent, mouseOutContent){
-//    elem.addEventListener("mouseover", function(){
-//        elem.innerHTML = mouseOverContent;
-//    });
-//    elem.addEventListener("mouseout", function(){
-//        elem.innerHTML = mouseOutContent;
-//    });
-//}
-//
-//var oneClickUpload = {
-//    title: "OneClickUpload",
-//    htmlContent: '<div><p>Project OneClickUpload</p><img src="./img/skills/Android_Icon.png"/><img src="./img/skills/Java_Icon.png"/><img src="./img/skills/SQL_Icon.png"/></div>',
-//    htmlContentHover: '<div><p>Android app to upload photo to multiple social media platforms simutaneously</p><ul>Technology: <li>Android</li><li>Java</li><li>SQLite Database</li></ul></div>'
-//};
-//
-//var portfoliov1 = {
-//    title: "Portfolio V1",
-//    htmlContent: '<div><p>Project Portfolio v1</p><img src="./img/skills/HTML_Icon.png"/><img src="./img/skills/CSS_Icon.png"/><img src="./img/skills/JS_Icon.png"/><img src="./img/skills/Bootstrap_Icon.png"/></div>',
-//    htmlContentHover:  '<div><p>The very first responsive Bootstrap website to serve as my web portfolio.</p><ul>Technology: <li>HTML5</li><li>CSS</li><li>JavaScript</li><li>Bootstrap</li></ul></div>'
-//};
-//
-//var mathRiceball = {
-//    title: "Math Riceball",
-//    htmlContent: '<div><p>Project Math Riceball</p><img src="./img/skills/UNITY_Icon.png"/><img src="./img/skills/CSharp_Icon.png"/></div>',
-//    htmlContentHover: '<div><p>An Unity Game created during 2015 Windows 10 Game Jam by a group of four students </p><ul>Technology: <li>Unity</li><li>C#</li><li>Visual Studio</li></ul></div>'
-//};
-//
-//var parkNEat = {
-//    title: "ParkNEat",
-//    htmlContent: '<div><p>Project ParkNEat</p><img src="./img/skills/Java_Icon.png"/><img src="./img/skills/SQL_Icon.png"/></div>',
-//    htmlContentHover: '<div><p>A Web Application to display restaurants along with their ratings/comments on Google Map and nearby parking spots.</p><ul>Technology: <li>Java</li><li>NoSQL database</li><li>Google Web Toolkit</li></ul></div>'
-//};
-//
-//var shopOnline = {
-//    title: "Shop Online!",
-//    htmlContent: '<div><p>Project Shop Online!</p><img src="./img/skills/PHP_Icon.png"/><img src="./img/skills/SQL_Icon.png"/><img src="./img/skills/HTML_Icon.png"/></div>',
-//    htmlContentHover: '<div><p>A Website that serves as an online shopping platform and inventory management system.</p><ul>Technology: </ul><li>PHP</li><li>MySQL database</li><li>HTML5</li></div>'
-//
-//};
-//
-//var projects = [oneClickUpload, portfoliov1, mathRiceball, parkNEat, shopOnline];
+/**
+ * The function to draw one portion of the pie chart
+ * @param ctx       :   Context of the canvas
+ * @param centerX   :   The X coordinate of the center of the pie chart
+ * @param centerY   :   The Y coordinate of the center of the pie chart
+ * @param radius    :   Radius of the pie chart
+ * @param startAngle:   The starting angle of the pie slice
+ * @param endAngle  :   The ending angle of the pie slice
+ * @param color     :   The color of the slice
+ */
+function drawPieSlice(ctx, centerX, centerY, radius, startAngle, endAngle, color){
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(centerX, centerY);
+    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+    ctx.closePath();
+    ctx.fill();
+}
+
+/**
+ * The constructor of the pie chart
+ * options parameters contains the following:
+ *      canvas  :   The Canvas DOM object to draw PieChart in
+ *      data    :   The data of the pie chart, containing the proportion of each pie slice
+ *      colors  :   The colors to be used in the pie chart
+ *      legend  :   The DIV DOM object to contain the legend
+ */
+var PieChart = function(options){
+    //Saving variables
+    this.options = options;
+    this.canvas = options.canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.colors = options.colors;
+
+    /**
+     * The draw function of the class
+     * is to draw the whole pie chart and add legends to the DIV DOM object (legend)
+     */
+    this.draw = function(){
+        var totalValue = 0;
+        var color_index = 0;
+        // Summing up the total values of the pie chart
+        for(var categ in this.options.data){
+            var val = this.options.data[categ];
+            totalValue += val;
+        }
+
+        var start_angle = 0;
+        // For each pie section
+        for(categ in this.options.data){
+            val = this.options.data[categ];
+            // Measure the angle of the section
+            var slice_angle = 2 * Math.PI * val / totalValue;
+
+            // Determine the location of the text for the section
+            var pieRadius = Math.min(this.canvas.width/2, this.canvas.height/2);
+            var labelX = this.canvas.width/2 + (pieRadius / 2) * Math.cos(start_angle + slice_angle/2);
+            var labelY = this.canvas.height/2 + (pieRadius / 2 ) * Math.sin(start_angle + slice_angle/2);
+
+            // Draw the slice
+            drawPieSlice(this.ctx, this.canvas.width/2, this.canvas.height/2, Math.min(this.canvas.width/2, this.canvas.height/2), start_angle, start_angle+slice_angle, this.colors[color_index%this.colors.length]);
+
+            // Draw the text for the section
+            var labelText = Math.round(100 * val / totalValue);
+            this.ctx.fillStyle = "black";
+            this.ctx.font = "bold 20px Arial";
+            this.ctx.fillText(labelText + "%", labelX, labelY);
+
+            //Increment the start_angle and color index
+            start_angle += slice_angle;
+            color_index++;
+        }
+
+        // If legend is included in the options
+        if(this.options.legend){
+            color_index = 0;
+            var legendHTML = "";
+            // For each section
+            for(categ in this.options.data){
+                // Add the color and the name of the section to the legend
+                legendHTML += '<div><span style="display:inline-block;width:20px;background-color:'+this.colors[color_index++]+';">&nbsp;</span> ' + categ + '</div>';
+                this.options.legend.innerHTML = legendHTML;
+            }
+        }
+    }
+}
+
+/**
+ * Simple function to get the corresponding pie chart data with ID
+ * @param id    : The ID of the Canvas DOM object
+ * @returns {*} : The data for the pie chart
+ */
+function getData(id){
+    if(id == "OneClickUploadPie"){
+        return OneClickUpload;
+    } else {
+        return null;
+    }
+}
